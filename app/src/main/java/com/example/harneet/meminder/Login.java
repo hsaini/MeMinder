89dca01b2221cdb742695f4e14771d97ea43164c
Harneet Kaur Saini <Harneetsaini@hotmail.ca>
2017-12-29 10:41:09 -0500

    Add code to Login java file

--- app/src/main/java/com/example/harneet/meminder/Login.java
+++ app/src/main/java/com/example/harneet/meminder/Login.java
@@ -1,13 +1,64 @@
 package com.example.harneet.meminder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import java.util.List;
+
+public class Login extends Activity implements OnClickListener {
+
+    private Button logInButton;
+    private Button createAccountButton;
+    private EditText userName;
+    private EditText password;
+
+    private User_Login user_login;
+    private User_Login_AppDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        database = User_Login_AppDatabase.getDatabase(getApplicationContext());
+        database.user_login_dao().removeAllUsers();
+
+        List<User_Login> user_logins = database.user_login_dao().getAllUser();
+        if (user_logins.size() == 0) {
+            database.user_login_dao().addUser(new User_Login(1, "Hsaini7844", "123456"));
+            user_login = database.user_login_dao().getAllUser().get(0);
+        }
+
+        userName = (EditText) findViewById(R.id.userNameEditLabel);
+        password = (EditText) findViewById(R.id.passwordEditText);
+
+
+        logInButton = (Button)
+                findViewById(R.id.logInButton);
+        createAccountButton = (Button)
+                findViewById(R.id.createAccountButton);
+
+        logInButton.setOnClickListener(this);
+        createAccountButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.logInButton){
+            Intent login = new Intent(Login.this,
+                    Home.class);
+            startActivity(login);
+        }
+        else if(v.getId() == R.id.createAccountButton){
+            Intent create = new Intent(Login.this,
+                    Create_Account.class);
+            startActivity(create);
+        }
     }
 }

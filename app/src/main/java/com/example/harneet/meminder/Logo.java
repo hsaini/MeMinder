a3b6f73484b54a25245fe8e060bcc7aeaf163e77
Harneet Kaur Saini <Harneetsaini@hotmail.ca>
2017-12-31 10:42:03 -0500

    Add code for button in Logo java file

--- app/src/main/java/com/example/harneet/meminder/Logo.java
+++ app/src/main/java/com/example/harneet/meminder/Logo.java
@@ -1,13 +1,34 @@
 package com.example.harneet.meminder;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Logo extends AppCompatActivity {
+public class Logo extends Activity implements OnClickListener {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        start = (Button)
+                findViewById(R.id.button_start);
+
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_start){
+            Intent getstarted = new Intent(Logo.this,
+                    Login.class);
+            startActivity(getstarted);
+        }
     }
 }

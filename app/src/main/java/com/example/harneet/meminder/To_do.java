d32b4fc92da5e03a083ac28bf419c4ae995da107
Harneet Kaur Saini <Harneetsaini@hotmail.ca>
2018-01-03 23:05:42 -0500

    Added a back home button to To_do file

--- app/src/main/java/com/example/harneet/meminder/To_do.java
+++ app/src/main/java/com/example/harneet/meminder/To_do.java
@@ -1,13 +1,33 @@
 package com.example.harneet.meminder;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class To_do extends AppCompatActivity {
+public class To_do extends Activity implements View.OnClickListener {
+
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
+
+        home = (Button)
+                findViewById(R.id.button_back_home);
+
+        home.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.button_to_do){
+            Intent back = new Intent(To_do.this, Home.class);
+            startActivity(back);
+        }
+
     }
 }

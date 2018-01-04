22d4ed559d762e981f8c325de66cd0299affb91f
Harneet Kaur Saini <Harneetsaini@hotmail.ca>
2018-01-03 23:37:13 -0500

    Back home button added to file

--- app/src/main/java/com/example/harneet/meminder/Calendar.java
+++ app/src/main/java/com/example/harneet/meminder/Calendar.java
@@ -1,13 +1,33 @@
 package com.example.harneet.meminder;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Calendar extends AppCompatActivity {
+public class Calendar extends Activity implements View.OnClickListener {
+
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        home = (Button)
+                findViewById(R.id.button_back_home);
+
+        home.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.button_to_do){
+            Intent back = new Intent(Calendar.this, Home.class);
+            startActivity(back);
+        }
+
     }
 }
